78e1f519d2a4d12832974ec88a4bc7f2c667d96b
engayarami <ayaghanem64@gmail.com>
2025-02-19 12:47:46 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ Route::get('/about', function(){
         '2'=>'Financial',
         '3'=>'Sales',
     ];
+    //return view('about', ['name' => $name]);
     //return view('about')->with('name',$name);
     return view('about', compact('name', 'departments'));
-    //return view('about', ['name' => $name]);
 });
 
 Route::post('/about', function(){
